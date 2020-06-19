50180d423b009778c7950625620bf6bd7f310b95
EvenyYlzz <172463074.com>
2020-06-19 15:20:09 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,17 @@
 <template>
-<div>helloword</div>
+<div>
+  <home-header>
+  </home-header>
+</div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="header">
+   <div class="header-left">
+       <div class="iconfont back-icon">&#xe624;</div>
+   </div>
+   <div class="header-input">
+       <span class="iconfont">&#xe632;</span>
+       输入城市/景点/游玩主题
+   </div>
+   <div class="header-right">
+       城市
+       <span class="iconfont arrow-icon">&#xe6aa;</span>
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+/* scoped限制对当前组件有效 */
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display : flex
+    background :$bgColor
+    line-height : .86rem
+    color  : #fff
+    .header-left
+        width:.64rem
+        float: left
+        .back-icon
+            text-align :center
+            font-size :.0.36rem
+    .header-input
+        flex:1
+        padding-left :.2rem
+        height : .64rem
+        line-height :.64rem
+        margin-top :.12rem
+        margin-left : .2rem
+        background :#fff
+        border-radius:.1rem
+        color :#ccc
+    .header-right
+        width:1.24rem
+        float : right
+        text-align:center
+        .arrow-icon
+            margin-left :-.04rem
+            font-size :.27rem
+</style>
