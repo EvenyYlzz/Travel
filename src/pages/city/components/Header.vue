c4ea78d1a51a27d71f7be45ca7f9726c196cf637
EvenyYlzz <172463074.com>
2020-06-21 10:25:41 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+        <router-link to="/">
+           <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+      城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .header
+        position:relative
+        height :$headerHeight
+        overflow :hidden
+        line-height :$headerHeight
+        text-align :center
+        color:#fff
+        background :$bgColor
+        .header-back
+            position:absolute
+            top:0
+            left:0
+            width :.64rem
+            font-size:.4rem
+            text-align :center
+            color:#fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
        <span class="iconfont">&#xe632;</span>
        输入城市/景点/游玩主题
    </div>
+   <router-link to='/city'>
    <div class="header-right">
        {{this.city}}
        <span class="iconfont arrow-icon">&#xe6aa;</span>
    </div>
+   </router-link>
 </div>
 </template>
 
@@ -29,7 +31,7 @@ export default {
   .header
     display : flex
     background :$bgColor
-    line-height : .86rem
+    line-height : $headerHeight
     color  : #fff
     .header-left
         width:.64rem
@@ -51,6 +53,7 @@ export default {
         width:1.24rem
         float : right
         text-align:center
+        color: #fff
         .arrow-icon
             margin-left :-.04rem
             font-size :.27rem
