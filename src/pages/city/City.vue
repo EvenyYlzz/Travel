a97a4b584609602a8fa26977e2c670977452a04f
EvenyYlzz <172463074.com>
2020-06-21 18:01:25 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -4,9 +4,9 @@
       </city-header>
       <city-search>
       </city-search>
-      <city-list>
+      <city-list :cities="cities" :hotCities="hotCities">
       </city-list>
-      <city-alphabet>
+      <city-alphabet :cities="cities">
       </city-alphabet>
   </div>
 </template>
@@ -16,7 +16,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
-
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -24,6 +24,29 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,78 +17,23 @@
                 热门城市
             </div>
             <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+                <div class="button-wrapper"  v-for="item of hotCities" :key="item.id">
+                    <div class="button">{{item.name}}</div>
                 </div>
             </div>
         </div>
-        <div class="area">
+        <div class="area" v-for="(item , key) of cities" :key="key">
             <div class="title border-topbottom">
-                    A
+                    {{key}}
                 </div>
                 <div class="item-list">
-                    <div class="item border-bottom">
-                        啊拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        啊拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        啊拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        啊拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        啊拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        啊拉尔
+                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+                        {{innerItem.name}}
                     </div>
             </div>
           </div>
       </div>
-      <div class="area">
-          <div class="title border-topbottom">
-                A
-              </div>
-              <div class="item-list">
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-          </div>
-      </div>
-      <div class="area">
-          <div class="title border-topbottom">
-                A
-              </div>
-              <div class="item-list">
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-                  <div class="item border-bottom">
-                      啊拉尔
-                  </div>
-          </div>
-    </div>
+
     </div>
   </div>
 </template>
@@ -97,6 +42,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hotCities: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
